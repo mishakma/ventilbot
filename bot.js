f836924765da709ac101c10f85f8bb9a15e3c5af
mishakma <45147490+mishakma@users.noreply.github.com>
2018-11-18 19:12:19 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,19 @@
+/*
+ * VentilBOT
+ * Version 1.0.0
+ * Michael Korolev
+ */
+
+const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
+
+if(!process.send) {
+
+Files.initStandalone();
+
+} else {
+
+process.on('message', function(content) {
+	Files.initBotTest(content);
+});
+
+}
